f79873ccef5741a0bfd281af9028ca6ff68f76c5
Monica Riegel <monicariegel@hotmail.com>
2024-07-04 23:10:06 -0300

    Implementando menu lateral

--- src/componentes/BarraLateral/index.jsx
+++ src/componentes/BarraLateral/index.jsx
@@ -1,4 +1,5 @@
 import { styled } from "styled-components"
+import ItensNavegacao from "./ItensNavegação"
 
 const ListaEstilizada = styled.ul`
     list-style: none;
@@ -12,11 +13,37 @@ const BarraLateral = () => {
         <aside>
             <nav>
                 <ListaEstilizada>
-                    <li>
-                        <a href="">
-                            Inicio
-                        </a>
-                    </li>
+                    <ItensNavegacao
+                        iconeAtivo="/icones/home-ativo.png"
+                        iconeInativo="/icones/home-inativo.png"
+                        ativo={true}
+                    >
+                        Inicio
+                    </ItensNavegacao>
+                    <ItensNavegacao
+                        iconeAtivo="/icones/mais-vistas-ativo.png"
+                        iconeInativo="/icones/mais-vistas-inativo.png"
+                    >
+                        Mais vistos
+                    </ItensNavegacao>
+                    <ItensNavegacao
+                        iconeAtivo="/icones/mais-curtidas-ativo.png"
+                        iconeInativo="/icones/mais-curtidas-inativo.png"
+                    >
+                        Mais curtidas
+                    </ItensNavegacao>
+                    <ItensNavegacao
+                        iconeAtivo="/icones/novas-ativo.png"
+                        iconeInativo="/icones/novas-inativo.png"
+                    >
+                        Novas
+                    </ItensNavegacao>
+                    <ItensNavegacao
+                        iconeAtivo="/icones/surpreenda-me-ativo.png"
+                        iconeInativo="/icones/surpreenda-me-inativo.png"
+                    >
+                        Surpreenda-me
+                    </ItensNavegacao>
                 </ListaEstilizada>
             </nav>
         </aside>
